01e692a3337db59ecb79a58245cd31a160f57b1d
Bertuğ Kalkan <kalkanbertug@gmail.com>
2025-06-14 23:05:00 +0300

    ilk değişiklikler

--- api/app.js
+++ api/app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) => {
+  console.log("Merhaba Dünya")
+  next()})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
